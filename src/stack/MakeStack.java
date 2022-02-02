c4eda7ba7b3187f2c1831a8f76b8a34cabc97a49
ksg0605 <ksg0605@gmail.com>
2022-02-02 17:15:21 +0900

    add Stack

---
+++ src/stack/MakeStack.java
@@ -0,0 +1,31 @@
+// 쉽게 해결함
+
+package stack;
+
+import java.util.ArrayList;
+
+public class MakeStack<T> {
+    private ArrayList<T> array = new ArrayList<T>();
+
+    public void push(T item) {
+        array.add(item);
+    }
+
+    public T pop() {
+        Integer size = array.size();
+        if (array.isEmpty()) {
+            return null;
+        }
+        return array.remove(size-1);
+    }
+
+    public static void main(String[] args) {
+        MakeStack<Integer> makeStack = new MakeStack<Integer>();
+        makeStack.push(1);
+        makeStack.push(2);
+        makeStack.push(3);
+        System.out.println(makeStack.pop());
+        System.out.println(makeStack.pop());
+        System.out.println(makeStack.pop());
+    }
+}
