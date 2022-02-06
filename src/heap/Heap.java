f989ca0c5e88f2a72d76307693d1935850e70060
ksg0605 <ksg0605@gmail.com>
2022-02-06 18:08:21 +0900

    add maxHeap

---
+++ src/heap/Heap.java
@@ -0,0 +1,130 @@
+package heap;
+
+import java.util.ArrayList;
+import  java.util.Collections;
+
+public class Heap {
+    public ArrayList<Integer> heapArray = null;
+
+    public Heap(Integer data) {
+        this.heapArray = new ArrayList<Integer>();
+        this.heapArray.add(null);
+        this.heapArray.add(data);
+    }
+
+    public boolean move_up(Integer inserted_idx) {
+        if (inserted_idx <= 1) {
+            return false;
+        }
+        Integer parent_idx = inserted_idx / 2;
+        if (this.heapArray.get(inserted_idx) > this.heapArray.get(parent_idx)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean insert(Integer data) {
+        Integer inserted_idx, parent_idx;
+        if (this.heapArray == null) {
+            this.heapArray = new ArrayList<Integer>();
+            this.heapArray.add(null);
+            this.heapArray.add(data);
+            return true;
+        }
+
+        this.heapArray.add(data);
+        inserted_idx = this.heapArray.size() - 1;
+
+        while (this.move_up(inserted_idx)) {
+            parent_idx = inserted_idx / 2;
+            Collections.swap(heapArray, inserted_idx, parent_idx);
+            inserted_idx = parent_idx;
+        }
+        return true;
+    }
+
+    public boolean move_down(Integer popped_idx) {
+        Integer left_child_popped_idx, right_child_popped_idx;
+
+        left_child_popped_idx = popped_idx * 2;
+        right_child_popped_idx = popped_idx * 2 + 1;
+
+        // CASE1: 왼쪽 자식 노드도 없을 때
+        if (left_child_popped_idx >= this.heapArray.size()) {
+            return false;
+            // CASE2: 오른쪽 자식 노드만 없을 때
+        } else if (right_child_popped_idx >= this.heapArray.size()) {
+            if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (this.heapArray.get(left_child_popped_idx) > this.heapArray.get(right_child_popped_idx)) {
+                if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if (this.heapArray.get(popped_idx) < this.heapArray.get(right_child_popped_idx)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    public Integer pop() {
+        Integer returned_data, popped_idx, left_child_popped_idx, right_child_popped_idx;
+
+        if (this.heapArray.size() <= 1) {
+            return null;
+        }
+
+        returned_data = this.heapArray.get(1);
+        this.heapArray.set(1, this.heapArray.get(this.heapArray.size() - 1));
+        this.heapArray.remove(this.heapArray.size() - 1);
+        popped_idx = 1;
+
+        while (this.move_down(popped_idx)) {
+            left_child_popped_idx = popped_idx * 2;
+            right_child_popped_idx = popped_idx * 2 + 1;
+
+            if (right_child_popped_idx >= this.heapArray.size()) {
+                if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+                    Collections.swap(this.heapArray, popped_idx, left_child_popped_idx);
+                    popped_idx = left_child_popped_idx;
+                }
+            } else {
+                if (this.heapArray.get(left_child_popped_idx) > this.heapArray.get(right_child_popped_idx)) {
+                    if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+                        Collections.swap(this.heapArray, popped_idx, left_child_popped_idx);
+                        popped_idx = left_child_popped_idx;
+                    }
+                } else {
+                    if (this.heapArray.get(popped_idx) < this.heapArray.get(right_child_popped_idx)) {
+                        Collections.swap(this.heapArray, popped_idx, right_child_popped_idx);
+                        popped_idx = right_child_popped_idx;
+                    }
+                }
+            }
+        }
+        return returned_data;
+    }
+
+    public static void main(String[] args) {
+        Heap heapTest = new Heap(15);
+        heapTest.insert(10);
+        heapTest.insert(8);
+        heapTest.insert(5);
+        heapTest.insert(4);
+        heapTest.insert(20);
+        System.out.println(heapTest.heapArray);
+
+        heapTest.pop();
+        System.out.println(heapTest.heapArray);
+    }
+}
