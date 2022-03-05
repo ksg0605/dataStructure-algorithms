f47ca31eef17452ed2cbfb0171900190022949e7
ksg0605 <ksg0605@gmail.com>
2022-03-05 22:42:33 +0900

    add boj15652

--- src/solve/boj15652/solve.py
+++ src/solve/boj15652/solve.py
@@ -1,21 +1,21 @@
-import  sys
+import sys
 
 n, m = map(int, sys.stdin.readline().split(' '))
 selected = [0 for _ in range(m)]
+used = [0 for _ in range(n + 1)]
 
 def rec_func(k):
+  # 탐색 끝남
   if k == m:
     for x in selected:
       sys.stdout.write(str(x) + ' ')
     sys.stdout.write('\n')
+  # 탐색 안끝남
   else:
-    start = 1 if k == 0 else selected[k-1]
-    for cand in range(start, n + 1):
-      if start > 1:
-        selected[k] = cand
-      selected[k] = cand
+    start = 1 if k == 0 else selected[k - 1]
+    for candidate in range(start, n+1):
+      selected[k] = candidate
       rec_func(k + 1)
-      start = 1
       selected[k] = 0
 
 rec_func(0)
